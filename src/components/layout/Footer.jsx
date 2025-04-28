a2b9228a5197f9c37fa0e6868056f3322c137694
Mohamed Fawzy Negm <157843257+Mohamedfawzynegm@users.noreply.github.com>
2025-04-29 02:28:22 +0300

    Update Footer.jsx

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -95,7 +95,7 @@ const Footer = () => {
               <h3 className="text-lg font-semibold text-white mb-4 md:mb-6">اتصل بنا</h3>
               <ul className="space-y-4">
                 {[
-                  { icon: <FaPhone />, text: '+20 123 456 789' },
+                  { icon: <FaPhone />, text: '+20 106 530 9906' },
                   { icon: <FaMapMarkerAlt />, text: 'أبو حماد، الشرقية، مصر' },
                   { icon: <FaEnvelope />, text: 'info@resala-abuhammad.org' }
                 ].map((contact, index) => (
